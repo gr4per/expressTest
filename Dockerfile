aaaec5e00da7e1bf3258421bea678af09cbb740a
Arne Gräper <github@arne-graeper.de>
2016-11-30 11:41:14 +0100

    Initial set of files to start playing

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM mhart/alpine-node:5
+MAINTAINER gr4per
+
+#create app directory
+RUN mkdir -p /app
+WORKDIR /app
+
+#Install app dependencies
+ADD ./package.json .
+
+RUN if [ $HTTP_PROXY ]; then echo "proxy set: " $HTTP_PROXY; else echo "proxy not set"; fi
+RUN if [ $HTTP_PROXY ]; then npm config set proxy "$HTTP_PROXY"; else npm config set proxy; fi
+RUN if [ $HTTPS_PROXY ]; then echo "https proxy set: " $HTTPS_PROXY; else echo "https proxy not set"; fi
+RUN if [ $HTTPS_PROXY ]; then npm config set https-proxy "$HTTPS_PROXY"; else npm config set https-proxy; fi
+RUN npm install -g supervisor 
+RUN npm install
+
+#Bundle app source
+#COPY . /usr/src/app
+
+EXPOSE 3000
+
+CMD [ "npm", "start" ]
+
---
+++ app.js
@@ -0,0 +1,63 @@
+var consul = require('consul')({ host: 'consul' })
+consul.catalog.service.list(function (err, result) {
+  if (err) throw err
+  console.log('services found: ' + result)
+})
+var mysqlHost;
+var mysqlPort;
+consul.catalog.service.nodes('mysql', function (err, result) {
+  if (err) throw err
+  console.log('mysql found: ' + JSON.stringify(result))
+  mysqlHost = result[0].ServiceAddress
+  mysqlPort = result[0].ServicePort
+  console.log('using mysql @ ' + mysqlHost + ':' + mysqlPort)
+})
+
+var express = require('express')
+var path = require('path')
+var favicon = require('serve-favicon')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+
+var index = require('./routes/index')
+var users = require('./routes/users')
+
+var app = express()
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+
+// uncomment after placing your favicon in /public
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+console.log('__dirname =\'' + __dirname + '\'')
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', index)
+app.use('/users', users)
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
+
+console.log('init done')
+module.exports = app
