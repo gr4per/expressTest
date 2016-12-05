b4f4805ffa5818f1301ebdaf7d51a145a147b7ce
gr4per <arne.graeper@opuscapita.com>
2016-12-05 15:49:50 +0100

    fixing az

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bash
+a#!/usr/bin/env bash
 
 # more bash-friendly output for jq
 JQ="jq --raw-output --exit-status"
 
 configure_aws_cli(){
 	aws --version
-	aws configure set default.region us-east-1
+	aws configure set default.region eu-central-1
 	aws configure set default.output json
 }
 
